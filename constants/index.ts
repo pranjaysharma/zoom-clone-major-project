cf8988ea9b3f9f790c2418eb50f0bad6ced1f83c
Pranjay <pranjay.sharma@gmail.com>
2024-05-19 13:14:11 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,40 +1,36 @@
 export const sidebarLinks = [
+  {
+    imgURL: '/icons/Home.svg',
+    route: '/',
+    label: 'Home',
+  },
 
-    {
-        label: 'Home',
-        route: '/',
-        imgUrl: '/icons/Home.svg',
-    },
-
-    {
-        label: 'Upcoming',
-        route: '/upcoming',
-        imgUrl: '/icons/Upcoming.svg',
-    },
-
-    {
-        label: 'Previous',
-        route: '/previous',
-        imgUrl: '/icons/Previous.svg',
-    },
-
-    {
-        label: 'Recordings',
-        route: '/recordings',
-        imgUrl: '/icons/Recordings.svg',
-    },
-
-    {
-        label: 'Personal Room',
-        route: '/personal-room',
-        imgUrl: '/icons/add-personal.svg',
-    },
-]
+  {
+    imgURL: '/icons/upcoming.svg',
+    route: '/upcoming',
+    label: 'Upcoming',
+  },
+  {
+    imgURL: '/icons/previous.svg',
+    route: '/previous',
+    label: 'Previous',
+  },
+  {
+    imgURL: '/icons/Video.svg',
+    route: '/recordings',
+    label: 'Recordings',
+  },
+  {
+    imgURL: '/icons/add-personal.svg',
+    route: '/personal-room',
+    label: 'Personal Room',
+  },
+];
 
 export const avatarImages = [
-    '/images/avatar-1.jpeg',
-    '/images/avatar-2.jpeg',
-    '/images/avatar-3.png',
-    '/images/avatar-4.png',
-    '/images/avatar-5.png',
-]
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
+];
