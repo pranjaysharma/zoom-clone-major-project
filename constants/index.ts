c8733f175fc3f7af3e9ecae894b731895600f6b4
Pranjay <pranjay.sharma@gmail.com>
2024-05-19 13:17:32 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,27 +1,27 @@
 export const sidebarLinks = [
   {
-    imgURL: '/icons/Home.svg',
+    imgUrl: '/icons/Home.svg',
     route: '/',
     label: 'Home',
   },
 
   {
-    imgURL: '/icons/upcoming.svg',
+    imgUrl: '/icons/upcoming.svg',
     route: '/upcoming',
     label: 'Upcoming',
   },
   {
-    imgURL: '/icons/previous.svg',
+    imgUrl: '/icons/previous.svg',
     route: '/previous',
     label: 'Previous',
   },
   {
-    imgURL: '/icons/Video.svg',
+    imgUrl: '/icons/Video.svg',
     route: '/recordings',
     label: 'Recordings',
   },
   {
-    imgURL: '/icons/add-personal.svg',
+    imgUrl: '/icons/add-personal.svg',
     route: '/personal-room',
     label: 'Personal Room',
   },
